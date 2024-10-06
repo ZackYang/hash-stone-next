92dd0cfc41dd0f34db9c23c254f210e4ab565784
Zacky <zackyang214@gmail.com>
2024-10-06 15:36:16 +1100

    Refactor file paths for home components and remove FAQ component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,6 +4,5 @@ export * from "./layout";
 export * from "./page";
 export * from "../components/home/our-stats";
 export * from "../components/home/about-services";
-export * from "../components/home/faq";
 export * from "./sponsored-by";
 export * from "../components/home/event-content";
